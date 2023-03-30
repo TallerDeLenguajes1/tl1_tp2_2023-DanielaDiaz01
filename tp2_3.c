7f020ee740feb035f44db190bf448c11f470e353
“Daniela <su>
2023-03-30 02:03:52 -0300

    punto 3 reimplementando codigo

---
+++ tp2_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Codigo utilizando aritmeica de punteros
+
+#define N 5
+#define M 7
+
+int main(){
+
+    int i,j;
+    int mt[N][M];
+    int *p = &mt[0][0];
+    
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++){
+        for(j = 0;j<M; j++){
+            *(p + i*M + j) = 1 + rand() % 100;
+            printf("%d", *(p + i*M + j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
